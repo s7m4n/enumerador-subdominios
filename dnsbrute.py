9c491e236a47cbb65a561c6d4b3dbf3ce6865c4b
s7m4n <63478331+s7m4n@users.noreply.github.com>
2020-06-28 17:19:46 -0300

    Add files via upload

---
+++ dnsbrute.py
@@ -0,0 +1,23 @@
+import sys
+import dns.resolver
+import pyfiglet
+
+def banner():
+    banner = pyfiglet.figlet_format('dnsbrute')
+    print(banner)
+
+def start():
+    try:
+        dominio = input('Digite o dominio sem http://:')
+        arquivo = open('dominios.txt','r+')
+        arqv = arquivo.read().splitlines()
+        for subdominio in arqv:
+            done = subdominio + '.' + dominio
+            conexao = dns.resolver.query(done, 'a')
+            print('Code 200 :', done, conexao)
+
+    except Exception:
+        pass
+
+banner()
+start()
