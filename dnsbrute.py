6579ce8ccd8a4c97cd926abbe08003ac3c71d137
Nelson Kenmochi <nelsonkenmochi@gmail.com>
2020-09-07 22:37:02 -0400

    change file read to "with as"
    I don't know exactly why, but my boss said It's more secure to used this way

--- dnsbrute.py
+++ dnsbrute.py
@@ -9,12 +9,13 @@ def banner():
 def start():
     try:
         dominio = input('Digite o dominio sem http://:')
-        arquivo = open('dominios.txt','r+')
-        arqv = arquivo.read().splitlines()
-        for subdominio in arqv:
-            done = subdominio + '.' + dominio
-            conexao = dns.resolver.query(done, 'a')
-            print('Code 200 :', done, conexao)
+        
+        with open('dominios.txt','r+') as arquivo:
+            arqv = arquivo.read().splitlines()
+            for subdominio in arqv:
+                done = subdominio + '.' + dominio
+                conexao = dns.resolver.query(done, 'a')
+                print('Code 200 :', done, conexao)
 
     except Exception as error:
         print ("Error: " + str(error))
